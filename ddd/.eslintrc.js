1624d7fd458f634507ff0bbec7d2a2c12f9b1374
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2022-08-24 13:34:45 -0300

    eslint

---
+++ ddd/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    node: true,
+    'jest/globals': true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['jest', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/prefer-default-export': 'off',
+  },
+};
---
+++ ddd/src/customer.spec.ts
@@ -0,0 +1,5 @@
+describe('', () => {
+  it('should', () => {
+    expect(1).toEqual(1);
+  });
+});
