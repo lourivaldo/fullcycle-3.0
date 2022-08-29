06ac812d89fd03e085263e4da22f01cf47d05a7b
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2022-08-28 21:54:45 -0300

    feat: added sequelize

---
+++ ddd/src/infrastructure/repository/product.repository.spec.ts
@@ -0,0 +1,25 @@
+import { Sequelize } from 'sequelize-typescript';
+import { ProductModel } from '../db/sequelize/model/product';
+
+describe('ProductRepository', () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: 'sqlite',
+      storage: ':memory:',
+      logging: false,
+      sync: { force: true },
+    });
+    sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it('should', () => {
+    expect('').toEqual('');
+  });
+});
