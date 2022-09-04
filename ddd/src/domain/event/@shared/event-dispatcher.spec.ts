36244479dbd02c851504cf6eeb34d90fd33cb76c
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2022-09-03 22:03:57 -0300

    feat: domain events notify handlers

--- ddd/src/domain/event/@shared/event-dispatcher.spec.ts
+++ ddd/src/domain/event/@shared/event-dispatcher.spec.ts
@@ -1,5 +1,9 @@
 import { EventDispatcher } from './event-dispatcher';
 import { SendEmailWhenProductIsCreatedHandler } from '../product/handler/send-email-when-product-is-created.handler';
+import { ProductCreatedEvent } from '../product/product-created.event';
+import MockDate from 'mockdate';
+
+MockDate.set(new Date('2022-09-04T00:58:18.956Z'));
 
 describe('EventDispatcher', () => {
   it('should register an event handler', async () => {
@@ -54,5 +58,32 @@ describe('EventDispatcher', () => {
     expect(
       eventDispatcher.getEventHandlers['ProductCreatedEvent'],
     ).toBeUndefined();
+  });
+
+  it('should notify all event handlers', async () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+    const eventHandlerSpy = jest.spyOn(eventHandler, 'handle');
+
+    eventDispatcher.register('ProductCreatedEvent', eventHandler);
+
+    expect(eventDispatcher.getEventHandlers['ProductCreatedEvent'][0]).toBe(
+      eventHandler,
+    );
+
+    const productCreatedEvent = new ProductCreatedEvent({
+      id: '1',
+      name: 'Product 1',
+    });
+    eventDispatcher.notify(productCreatedEvent);
+
+    expect(eventHandlerSpy).toHaveBeenCalledTimes(1);
+    expect(eventHandlerSpy).toHaveBeenCalledWith({
+      dataTimeOccurred: new Date('2022-09-04T00:58:18.956Z'),
+      eventData: {
+        id: '1',
+        name: 'Product 1',
+      },
+    });
   });
 });
