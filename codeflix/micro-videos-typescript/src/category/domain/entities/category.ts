674470554063eea36e02f04114606fc3231c37c0
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2022-09-10 15:21:20 -0300

    feat: category entity tests

--- codeflix/micro-videos-typescript/src/category/domain/entities/category.ts
+++ codeflix/micro-videos-typescript/src/category/domain/entities/category.ts
@@ -1,3 +1,41 @@
+
+type CategoryProperties = {
+  name: string
+  description?: string
+  isActive?: boolean
+  createdAt?: Date
+}
+
 export class Category {
-  constructor(public name: string) { }
+  constructor(
+    public readonly props: CategoryProperties
+  ) {
+    this.description = this.props.description
+    this.isActive = this.props.isActive
+    this.props.createdAt = this.props.createdAt || new Date()
+   }
+
+  get name() {
+    return this.props.name
+  }
+
+  get description() {
+    return this.props.description
+  }
+
+  get isActive() {
+    return this.props.isActive
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  private set description(value: string) {
+    this.props.description = value ?? null
+  }
+
+  private set isActive(value: boolean) {
+    this.props.isActive = value ?? true
+  }
 }
